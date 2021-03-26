76609582346659ffbf704c3468bc252163087285
Selcuk Cabuk <cabuk.selcuk@gmail.com>
2021-03-26 12:16:59 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import "semantic-ui-css/semantic.min.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
+
+
+class App extends React.Component {
+
+    state = {lat: null, errorMessage: ""}
+
+    componentDidMount() {
+        console.log("start of componentDidMount")
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat : position.coords.latitude}),
+            err => this.setState({errorMessage: err.message})
+        );
+    }
+
+    renderContent() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return (
+                <div>
+                    Error: {this.state.errorMessage}
+                </div>
+            );
+        }
+        if (!this.state.errorMessage && this.state.lat) {
+            return (
+                <SeasonDisplay lat={this.state.lat}/>
+            );
+        }
+
+        return (
+            <Spinner message="Please accept location request"/>
+        );
+    }
+
+
+    render() {
+        console.log("start of render")
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        )
+
+    }
+}
+
+// Take the react component and show it on the screen
+ReactDOM.render(<App/>, document.querySelector('#root'));
